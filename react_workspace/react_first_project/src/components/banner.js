c4f80960d8eecc033998c99d5577844ccb99c7d6
Aditya Kumar Sahu <adityasahu1124@gmail.com>
2022-04-23 22:34:33 +0530

    learning react components

---
+++ react_workspace/react_first_project/src/components/banner.js
@@ -0,0 +1,5 @@
+function Banner() {
+    return(<h2>Here is the banner</h2>)
+}
+
+export default Banner
---
+++ react_workspace/react_first_project/src/components/heading.js
@@ -0,0 +1,14 @@
+function Heading() {
+    return(
+        <>
+            <h1>Welcome to React</h1>
+            <p>React is very interesting. You'll love it</p>
+            <button>Click Me</button>
+            <div>
+                <h2>Hope you are fine</h2>
+            </div>
+        </>        
+    )
+}
+
+export default Heading
