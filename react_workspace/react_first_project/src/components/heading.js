8154b0375eab71aeef4369cf2fcb020b126c0c3a
Aditya Kumar Sahu <adityasahu1124@gmail.com>
2022-04-24 23:03:31 +0530

    learning function components

--- react_workspace/react_first_project/src/components/heading.js
+++ react_workspace/react_first_project/src/components/heading.js
@@ -1,8 +1,21 @@
+import './../styles/heading.css'
+
 function Heading() {
+
+    let isUserRegistered = true
+
+    let headingClass
+
+    if (isUserRegistered)
+        headingClass="main-heading"
+    else 
+        headingClass="main-heading-2"    
+
+
     return(
         <>
-            <h1>Welcome to React</h1>
-            <p>React is very interesting. You'll love it</p>
+            <h1 className={headingClass}>Welcome to React</h1>
+            <p className="para-style">React is very interesting. You'll love it</p>
             <button>Click Me</button>
             <div>
                 <h2>Hope you are fine</h2>
