4b5052fe24ac7e8d5f903363baa00e41d213003c
Aditya Kumar Sahu <adityasahu1124@gmail.com>
2022-04-27 21:26:11 +0530

    learning event handling

--- react_workspace/react_first_project/src/App.js
+++ react_workspace/react_first_project/src/App.js
@@ -1,13 +1,49 @@
+import { Component } from "react"
 import HeadingClass from "./components/headingClass"
-import Banner from "./components/banner"
 
-function App(){
-    return(
-        <>
-        <HeadingClass />
-        <Banner />
-        </>
-    )
+// function handleClick(){
+//     console.log("Button is Clicked")
+// }
+
+// function handleInputChange(){
+//     console.log("Someone is typing")
+// }
+
+// function onMouseEnter(){
+//     console.log("Mouse has entered")
+// }
+
+// function HandleFocus(){
+//     console.log("Input in Focus")
+// }
+
+class App extends Component{
+    handleClick(){
+        console.log("Button is Clicked")
+    }
+    
+    handleInputChange(){
+        console.log("Someone is typing")
+    }
+    
+    onMouseEnter(){
+        console.log("Mouse has entered")
+    }
+    
+    HandleFocus(){
+        console.log("Input in Focus")
+    }
+
+    render(){
+        return(
+            <>
+            <HeadingClass />
+            <button onClick={this.handleClick} id="button-1">Primary Button</button>
+            <button onClick={this.handleClick} id="button-2">Secondary Button</button>
+            <input onChange={this.handleInputChange} onMouseEnter={this.onMouseEnter} onFocus={this.HandleFocus}/>
+            </>
+        )
+    }
 }
 
 export default App
